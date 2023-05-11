ef8230c509d248e9c84ff9989aa1a2b2f3610e7c
Rezky Putra Barens <92147766+rezkybarensstudent@users.noreply.github.com>
2023-05-11 22:39:55 +0700

    Delete Dynamics of Particles and Rigid Bodies directory

--- Dynamics of Particles and Rigid Bodies/Principal Moments of Inertia/Principal_Moments_of_Inertia.m
+++
@@ -1,5 +0,0 @@
-% Principal Moments of Inertia
-Irot = [150, 0, -100; 0, 250, 0; -100, 0, 500];
-[PrincipalDirections, PrincipalMoments] = eig(Irot)
-TraceIrot = trace(Irot)
-TracePM = trace(PrincipalMoments)
